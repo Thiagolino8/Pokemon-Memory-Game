0bd05a2c521e3ef68faf462ac1fbd5d19962a509
Thiago Lino Gomes <thiagolinog8@gmail.com>
2023-04-06 18:57:26 -0300

    Fiz handle

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,7 @@
 import { minify } from 'html-minifier';
 import { building } from '$app/environment';
 import type { Handle } from '@sveltejs/kit';
- 
+
 const minification_options = {
   collapseBooleanAttributes: true,
   collapseWhitespace: true,
@@ -21,15 +21,15 @@ const minification_options = {
   sortClassName: true
 };
 
-export const handle = async (({ event, resolve }) => {
-  let page = '';
- 
-  return resolve(event, {
-    transformPageChunk: ({ html, done }) => {
-      page += html;
-      if (done) {
-        return building ? minify(page, minification_options) : page;
-      }
-    }
-  });
+export const handle = (async ({ event, resolve }) => {
+	let page = ''
+
+	return resolve(event, {
+		transformPageChunk: ({ html, done }) => {
+			page += html
+			if (done) {
+				return building ? minify(page, minification_options) : page
+			}
+		},
+	})
 }) satisfies Handle
