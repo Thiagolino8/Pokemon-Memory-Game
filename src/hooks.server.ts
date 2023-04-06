e89546313e743fa9d8fd6f3db26d6c6db7fc43f0
Thiago <thiagolinog8@gmail.com>
2023-04-06 00:26:29 -0300

    Fix minify

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,36 +1,35 @@
-import { prerendering } from '$app/environment'
-import type { Handle } from '@sveltejs/kit'
-import { minify } from 'html-minifier'
-
+import { minify } from 'html-minifier';
+import { building } from '$app/environment';
+import type { Handle } from '@sveltejs/kit';
+ 
 const minification_options = {
-	collapseBooleanAttributes: true,
-	collapseWhitespace: true,
-	conservativeCollapse: true,
-	decodeEntities: true,
-	html5: true,
-	ignoreCustomComments: [/^#/],
-	minifyCSS: true,
-	minifyJS: true,
-	removeAttributeQuotes: true,
-	removeComments: true,
-	removeOptionalTags: true,
-	removeRedundantAttributes: true,
-	removeScriptTypeAttributes: true,
-	removeStyleLinkTypeAttributes: true,
-	sortAttributes: true,
-	sortClassName: true,
-}
+  collapseBooleanAttributes: true,
+  collapseWhitespace: true,
+  conservativeCollapse: true,
+  decodeEntities: true,
+  html5: true,
+  ignoreCustomComments: [/^#/],
+  minifyCSS: true,
+  minifyJS: false,
+  removeAttributeQuotes: true,
+  removeComments: false, // some hydration code needs comments, so leave them in
+  removeOptionalTags: true,
+  removeRedundantAttributes: true,
+  removeScriptTypeAttributes: true,
+  removeStyleLinkTypeAttributes: true,
+  sortAttributes: true,
+  sortClassName: true
+};
 
-/** @type {import('@sveltejs/kit').Handle} */
-export const handle: Handle = async ({ event, resolve }) => {
-	const response = await resolve(event)
-
-	if (prerendering && response.headers.get('content-type') === 'text/html') {
-		return new Response(minify(await response.text(), minification_options), {
-			status: response.status,
-			headers: response.headers,
-		})
-	}
-
-	return response
-}
+export const handle = async (({ event, resolve }) => {
+  let page = '';
+ 
+  return resolve(event, {
+    transformPageChunk: ({ html, done }) => {
+      page += html;
+      if (done) {
+        return building ? minify(page, minification_options) : page;
+      }
+    }
+  });
+}) satisfies Handle
